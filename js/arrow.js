b910d9eb980de72fe0cca22faabb7f8b0e550aaa
taskok2 <k2mkt01@gmail.com>
2021-07-21 20:10:52 +0900

    21.07.21
    5강 숙제 완성

--- js/arrow.js
+++ js/arrow.js
@@ -57,7 +57,49 @@ function toggleDescription({target}){
 for(let i = 0 ; i < toggleBtns.length ; ++i)
     toggleBtns[i].addEventListener("click", toggleDescription);*/
 
+const btnAni={
+    clicked : [{transform: "translateY(0)"},
+    {transform: "translateY(-10px)"}],
+    opt : {
+        duration : 60,
+        easing : "linear",
+        iterations : 2,
+        direction : "alternate"
+    }
+}    
+
+const calForm = document.getElementById("calculate-container");
+
+calForm.addEventListener("submit", e=>{
+    e.preventDefault();
+});
+
+const calModeBtn = document.getElementById("calculate-mode");
+const calSubmit = document.getElementById("calculate-submit");
+const calMode = ["+", "-", "*", "/", "**"];
+
+function changeMode({target}){
+    let modeIdx = calMode.indexOf(target.innerText)+1;
+    if(1 > modeIdx || calMode.length <= modeIdx)
+        modeIdx = 0;
+    target.innerText = calMode[modeIdx];
+}
+
+calModeBtn.addEventListener("click", changeMode);
+
+function animateBtn ({target}){
+    target.animate(btnAni.clicked, btnAni.opt);
+}
+
+calModeBtn.addEventListener("click", animateBtn);
+calSubmit.addEventListener("click", animateBtn);
+
 function calculate(command, i, j){
+    if(isNaN(i) || isNaN(j))
+        return "";
+    if(999<i || 999 < j)
+        return "";    
+
     switch(command){
         case "add" :
         case "+" :
@@ -72,9 +114,17 @@ function calculate(command, i, j){
         case "/":
             return i/j;
         case "remainder":
-        case "**":
-            return i**j;
+        case "%":
+            return i%j;
         default:
             return "Command is wrong";                        
     }
 }
+
+const inputs = document.getElementsByClassName("calculate-input");
+
+function requestCalculate(){
+    inputs[2].value = calculate(calModeBtn.innerText, parseInt(inputs[0].value), parseInt(inputs[1].value));
+}
+
+calSubmit.addEventListener("click", requestCalculate);
